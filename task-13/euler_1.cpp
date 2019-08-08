fa807e79b2abfe9e47f622de60c0342b022ca2ac
Akshay V <akymaster007@gmail.com>
2019-08-09 00:33:25 +0530

    Add files via upload

---
+++ task-13/euler_1.cpp
@@ -0,0 +1,58 @@
+
+//The program gives correct output for 4/6 inputs
+
+#include<iostream>
+using namespace std;
+
+
+int last_term(int a,int n)
+    {
+        int l = n - 1;
+    while (l>0)
+    {
+        if(l%a == 0)
+         {
+                return l;
+            break;
+         }
+        l -= 1;
+    }    
+    return (-a);
+    }
+
+int no_of_terms(int a,int l,int d)
+    {
+        return ((l - a)/d +1);
+    }
+
+int ap(int a,int n,int l)
+    {
+        return ((n * (a + l))/2);
+    }
+
+int main()
+{
+     long long t;
+     long long int n;
+     long long  n_3,n_5,n_15;
+    cin>>t;
+     long long sum[100000];
+    for(int x=0;x<t;x++)
+    {
+        sum[x] = 0;
+        cin>>n;
+         n_3 = no_of_terms(3,last_term(3,n),3);
+         n_5 = no_of_terms(5,last_term(5,n),5);
+            n_15 = no_of_terms(15,last_term(15,n),15);
+            
+            //cout<<n_3<<"  "<<n_5<<"  "<<n_15<<endl;
+
+    sum[x] += ap(3,n_3,last_term(3,n)) + ap(5,n_5,last_term(5,n)) - ap(15,n_15,last_term(15,n));
+        //cout<<ap(3,n_3,last_term(3,n))<<"  "<<ap(5,n_5,last_term(5,n))<<"  "<<ap(15,n_15,last_term(15,n))<<endl;
+    }
+    for(int x=0;x<t;x++)
+    {
+        cout<<sum[x]<<endl;
+    }
+    
+}
---
+++ task-13/euler_3.cpp
@@ -0,0 +1,59 @@
+
+//The program runs correctly for 5/6 inputs and terminates due to timeout in one of the inputs.
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int primeorNot(int num)
+{
+    
+    for(int x=2;x<num;x++)
+    {
+        if(num%x==0)
+            {
+                return 0;
+            }
+    }
+        return 1;    
+}
+
+int main()
+{
+    int t;
+    long long n;
+    cin>>t;
+    int lar[10];
+    for(int x=0;x<t;x++)
+    {
+        cin>>n;
+        if(primeorNot(n))
+        {
+            lar[x]=n;
+            continue;
+        }
+        
+        lar[x] = 2;
+        for(int y=2;y<=n;y++)
+        {
+            if(n%y==0)
+            {
+                if(primeorNot(y))
+                {
+                    if(lar[x]<y)
+                        {lar[x]=y;
+                        n = n/y;
+                        }
+                        
+                }    
+            }
+        }
+        
+    }
+    for(int x=0;x<t;x++)
+    {
+        cout<<lar[x]<<endl;
+    }
+    
+}
