5728569dd65c8d52cd7e1a46eef77d9e34230c4d
Akshay V <akymaster007@gmail.com>
2019-08-11 16:14:44 +0530

    Improved the code

--- task-2/HackerRank/diagonal-difference.cpp
+++ task-2/HackerRank/diagonal-difference.cpp
@@ -4,36 +4,34 @@ using namespace std;
 
 int diagonalDifference(int arr[][100],int n)
 {
-	int diff;
-	int leftSum = 0,rightSum = 0;
-	for(int x=0;x<n;x++)
-	{
-		for(int y=0;y<n;y++)
-		{
-			if(x == y)
-				leftSum += arr[x][y];
-				
-		 	if(y == n-x-1)
-				rightSum += arr[x][y];
+    int diff;
+    int leftSum = 0,rightSum = 0,y;
+    for(int x=0;x<n;x++)
+    {
+        
+            y=x;
+            leftSum += arr[x][y];
+                
+            y=n-x-1;
+            rightSum += arr[x][y];
 
-		}
-	}
-	diff = abs(leftSum - rightSum);
-	return diff;
+    }
+    diff = abs(leftSum - rightSum);
+    return diff;
 }
 
 int main()
 {
-	int n,arr[100][100];
-	cin>>n;
-	
-	for(int x=0;x<n;x++)
-	{
-		for(int y=0;y<n;y++)
-		{
-			cin>>arr[x][y];
-		}
-	}
+    int n,arr[100][100];
+    cin>>n;
+    
+    for(int x=0;x<n;x++)
+    {
+        for(int y=0;y<n;y++)
+        {
+            cin>>arr[x][y];
+        }
+    }
 
-	cout<<diagonalDifference(arr,n);
+    cout<<diagonalDifference(arr,n);
 }
