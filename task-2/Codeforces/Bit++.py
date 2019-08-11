0ab06aa2adfc05f67329f2b94591586bda9a40dc
Akshay V <akymaster007@gmail.com>
2019-08-11 18:24:59 +0530

    Update Bit++.py

--- task-2/Codeforces/Bit++.py
+++ task-2/Codeforces/Bit++.py
@@ -3,14 +3,13 @@ a = []
 for i in range(n):
     s = input()
     a.append(s)
-
-
+ 
 x = 0
-
+ 
 for i in range(n):
     if "+" in a[i]:
-        x+=1
+        x += 1
     elif "-" in a[i]:
         x -= 1
-
+ 
 print(x)
