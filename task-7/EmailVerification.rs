fd4c9654c773449eed98233f4c480e88753d0c9e
akshayaky <akymaster@gmail.com>
2019-09-03 09:28:37 +0530

    Added input functionality

--- task-7/EmailVerification.rs
+++ task-7/EmailVerification.rs
@@ -4,17 +4,18 @@ use regex::Regex;
 fn main() {
 
     let re = Regex::new(r"^[[:words:][^@\s]]{1,64}@[A-Za-z0-9[^@\s]]+\.[a-zA-Z.-]+$").unwrap();
-    
-    let  mut email = "edward675@gmail.com";
-    
-    if re.is_match(&mut email) == true
+    println!("Enter Email id: ",);
+    let mut email = String::new();
+    std::io::stdin().read_line(&mut email).unwrap();
+    let mut email = email.trim();;
+    if (re.is_match(&mut email)) == true
     {
         println!("Email Address is Valid.",);
     }
        
     else
     {
-        println!("Email Address is invalid.")
+        println!("Email Address is invalid.");
     }
     
 }
